97a14c43c821b4bc388e00fff88154e9360ccc02
micki <micki.zhou@foxmail.com>
2021-06-02 12:41:49 +0800

    update

---
+++ lib/config/my_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static Color theme = Color(0xff5b5b5b);
+}
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,8 +1,8 @@
+import 'package:choose/config/my_colors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -11,8 +11,31 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: MyColors.theme,
+      body: Center(
+        child: Column(
+          children: [_addFoodInput(), _confirmButton()],
+        ),
+      ),
+    );
+  }
+
+  Widget _addFoodInput() {
+    return TextField(
+      onChanged: null,
+      decoration: InputDecoration(fillColor: Colors.white, filled: true),
+    );
+  }
+
+  Widget _confirmButton() {
+    return TextButton(
+      onPressed: null,
+      child: Text('确定'),
+      style: ButtonStyle(
+          textStyle: MaterialStateProperty.all(TextStyle(fontSize: 18)),
+          backgroundColor: MaterialStateProperty.all(Colors.blue),
+          foregroundColor: MaterialStateProperty.all(Colors.white)),
     );
   }
 }
